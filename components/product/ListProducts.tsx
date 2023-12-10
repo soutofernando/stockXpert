ffa992428ac7202c34076059f5ee32f57dd0175a
soutofernando <fernandosoutol45@gmail.com>
2023-12-10 02:32:23 -0300

    Force deploy

--- components/product/ListProducts.tsx
+++ components/product/ListProducts.tsx
@@ -152,7 +152,7 @@ export default function ListProducts(props: PageProps) {
               </table>
               <div class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
                 <span class="text-xs xs:text-sm text-gray-900">
-                  Showing 1 to 4 of 50 Entries
+                  Showing 1 to 4 of 50
                 </span>
                 <div class="inline-flex mt-2 xs:mt-0">
                   <button class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
