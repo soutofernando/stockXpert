1c1d9d0903a4217b6d24e86277325918cc14e638
soutofernando <fernandosoutol45@gmail.com>
2023-12-12 20:19:19 -0300

    partial implementation of the post

--- islands/ProductsList.tsx
+++ islands/ProductsList.tsx
@@ -6,6 +6,31 @@ export default function ProductsList(props: PageProps) {
   const { data } = props;
   const colums = Object.keys(data.data[0]);
   const open = useSignal(false);
+  const modalValues = useSignal({});
+
+  const newForm = () => {
+    const form = new FormData();
+    form.append("product_name", modalValues.value.product_name);
+    form.append("description", modalValues.value.description);
+    form.append("entry", modalValues.value.entry);
+    form.append("exit", modalValues.value.exit);
+
+    return form;
+  };
+
+  const updateProduct = async () => {
+    const response = await fetch("http://localhost:8000/auth/products", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(modalValues.value),
+    });
+
+    if (response.ok) {
+      window.location.reload();
+    }
+
+    return response;
+  };
 
   return (
     <div class="antialiased font-sans bg-gray-200">
@@ -133,7 +158,9 @@ export default function ProductsList(props: PageProps) {
                       </td>
                       <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                         <button
-                          onClick={() => open.value = true}
+                          onClick={() => {
+                            open.value = true, modalValues.value = row;
+                          }}
                           class="px-1 py-1 text-gray-900 transition-colors duration-200 rounded-lg dark:text-gray-900 hover:bg-gray-100"
                         >
                           <svg
@@ -156,7 +183,7 @@ export default function ProductsList(props: PageProps) {
                   ))}
                 </tbody>
               </table>
-              <div class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+              <div class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between">
                 <span class="text-xs xs:text-sm text-gray-900">
                   Showing 1 to 4 of 5
                 </span>
@@ -173,158 +200,23 @@ export default function ProductsList(props: PageProps) {
           </div>
         </div>
       </div>
+
       <Modal
         open={open.value}
       >
         <div role="alert" class="container mx-auto w-11/12 md:w-2/3 max-w-lg">
-          <div class="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
-            <div class="w-full flex justify-start text-gray-600 mb-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="icon icon-tabler icon-tabler-wallet"
-                width="52"
-                height="52"
-                viewBox="0 0 24 24"
-                stroke-width="1"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <path d="M17 8v-3a1 1 0 0 0 -1 -1h-10a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1 -1 1h-12a2 2 0 0 1 -2 -2v-12" />
-                <path d="M20 12v4h-4a2 2 0 0 1 0 -4h4" />
-              </svg>
-            </div>
-            <h1 class="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">
-              Enter 
-            </h1>
-            <label
-              for="name"
-              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
-            >
-              Product
-            </label>
-            <input
-              id="name"
-              class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
-              placeholder="James"
-            />
-            <label
-              for="email2"
-              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
-            >
-              Quantity
-            </label>
-            <div class="relative mb-5 mt-2">
-              <div class="absolute text-gray-600 flex items-center px-4 border-r h-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-credit-card"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <rect x="3" y="5" width="18" height="14" rx="3" />
-                  <line x1="3" y1="10" x2="21" y2="10" />
-                  <line x1="7" y1="15" x2="7.01" y2="15" />
-                  <line x1="11" y1="15" x2="13" y2="15" />
-                </svg>
-              </div>
-              <input
-                id="email2"
-                class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-16 text-sm border-gray-300 rounded border"
-                placeholder="XXXX - XXXX - XXXX - XXXX"
-              />
-            </div>
-            <label
-              for="expiry"
-              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
-            >
-              expory
-            </label>
-            <div class="relative mb-5 mt-2">
-              <div class="absolute right-0 text-gray-600 flex items-center pr-3 h-full cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-calendar-event"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <rect x="4" y="5" width="16" height="16" rx="2" />
-                  <line x1="16" y1="3" x2="16" y2="7" />
-                  <line x1="8" y1="3" x2="8" y2="7" />
-                  <line x1="4" y1="11" x2="20" y2="11" />
-                  <rect x="8" y="15" width="2" height="2" />
-                </svg>
-              </div>
-              <input
-                id="expiry"
-                class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
-                placeholder="MM/YY"
-              />
-            </div>
-            <label
-              for="cvc"
-              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
-            >
-              CVC
-            </label>
-            <div class="relative mb-5 mt-2">
-              <div class="absolute right-0 text-gray-600 flex items-center pr-3 h-full cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="icon icon-tabler icon-tabler-info-circle"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z"></path>
-                  <circle cx="12" cy="12" r="9"></circle>
-                  <line x1="12" y1="8" x2="12.01" y2="8"></line>
-                  <polyline points="11 12 12 12 12 16 13 16"></polyline>
-                </svg>
-              </div>
-              <input
-                id="cvc"
-                class="mb-8 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
-                placeholder="MM/YY"
-              />
-            </div>
-            <div class="flex items-center justify-start w-full">
-              <button class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">
-                Submit
-              </button>
-              <button
-                onClick={() => open.value = false}
-                class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm"
-              >
-                Cancel
-              </button>
-            </div>
+          <div
+            id="editProduct"
+            class="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400"
+          >
+            <input name="id" value={modalValues.value.id} class="hidden" />
             <button
               class="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out rounded focus:ring-2 focus:outline-none focus:ring-gray-600"
               aria-label="close modal"
               role="button"
-              onClick={() => open.value = false}
+              onClick={() => {
+                open.value = false, modalValues.value = {};
+              }}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -343,6 +235,107 @@ export default function ProductsList(props: PageProps) {
                 <line x1="6" y1="6" x2="18" y2="18" />
               </svg>
             </button>
+            <h1 class="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">
+              Editar produto: {modalValues.value.product_name}
+            </h1>
+            <label
+              for="name"
+              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
+            >
+              Produto
+            </label>
+            <input
+              type="text"
+              onChange={(e) =>
+                modalValues.value.product_name = (e.target as HTMLInputElement)
+                  ?.value}
+              value={modalValues.value.product_name}
+              id="productName"
+              class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+              placeholder="Ex: Bota"
+            />
+            <label
+              for="expiry"
+              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
+            >
+              Description
+            </label>
+
+            <input
+              onChange={(e) =>
+                modalValues.value.description = (e.target as HTMLInputElement)
+                  ?.value}
+              value={modalValues.value.description}
+              id="description"
+              class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+              placeholder="Ex: Tamnho 42"
+            />
+            <label
+              for="quantity"
+              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
+            >
+              Quantidade
+            </label>
+            <span
+              id="quantity"
+              class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+              placeholder="Ex: 2"
+            >
+              {modalValues.value.quantity}
+            </span>
+
+            <label
+              for="expiry"
+              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
+            >
+              Entrada
+            </label>
+
+            <input
+              onChange={(e) =>
+                modalValues.value.entry = (e.target as HTMLInputElement)
+                  ?.value}
+              value={modalValues.value.entry}
+              id="entry"
+              class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+              placeholder="Ex: 5"
+            />
+
+            <label
+              for="expiry"
+              class="text-gray-800 text-sm font-bold leading-tight tracking-normal"
+            >
+              Saída
+            </label>
+
+            <input
+              onChange={(e) =>
+                modalValues.value.exit = (e.target as HTMLInputElement)
+                  ?.value}
+              value={modalValues.value.exit}
+              id="entry"
+              class="text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border"
+              placeholder="Ex: 5"
+            />
+
+            <div class="flex items-center justify-start w-full pt-6">
+              <button
+                onClick={() => {
+                  updateProduct();
+                }}
+                class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm"
+              >
+                Atualizar Produto
+              </button>
+              <button
+                onClick={() => {
+                  open.value = false, modalValues.value = {};
+                }}
+                class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm"
+              >
+                Cancelar
+              </button>
+            </div>
           </div>
         </div>
       </Modal>
--- routes/auth/products.tsx
+++ routes/auth/products.tsx
@@ -3,8 +3,11 @@ import type { State } from "../_middleware.ts";
 import ProductsList from "../../islands/ProductsList.tsx";
 
 export const handler: Handlers<any, State> = {
-   async GET(_req, ctx){
-    const {data, error} = await ctx.state.supabaseClient.from("Stock").select("*")
+  async GET(_req, ctx) {
+    
+    const { data, error } = await ctx.state.supabaseClient.from("Stock").select(
+      "*",
+    );
     const headers = new Headers();
     let redirect = "/";
     if (error) {
@@ -12,10 +15,37 @@ export const handler: Handlers<any, State> = {
     }
 
     headers.set("location", redirect);
-    return ctx.render({data})
-   }
-  };
+    return await ctx.render({ data });
+  },
 
-export default function products (props: PageProps) {
-    return <ProductsList {... props} />
+  async PATCH(req, ctx) {
+    try {
+      const body = await req.body;
+      console.log(body)      
+      const { error } = await ctx.state.supabaseClient
+        .from("Stock")
+        .update({ product_name: "TEdsdSTE" })
+        .eq("id", 3);
+
+      const headers = new Headers();
+      const redirect = "/auth/secret";
+
+      if (!error) {
+        headers.set("location", redirect);
+      }
+
+      return new Response(null, {
+        status: 200,
+        headers,
+      });
+    } catch (error) {
+      console.error("Erro:", error.message);
+      // Você pode querer retornar um código de erro ou fazer um tratamento apropriado aqui
+      return new Response(null, { status: 500 });
+    }
+  },
+};
+
+export default function products(props: PageProps) {
+  return <ProductsList {...props} />;
 }
