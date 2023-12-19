2ce48b06f6a1dcf4a2b222539d7ba5230573ca6a
soutofernando <fernandosoutol45@gmail.com>
2023-12-19 12:34:03 -0300

    Dashboard UI

--- islands/PaymentsList.tsx
+++ islands/PaymentsList.tsx
@@ -27,8 +27,6 @@ export default function PaymentsList(props: PageProps) {
     return response;
   };
 
-  // data.forEach((payment) => total.value += payment.value);
-
   return (
     <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
       <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
